db514c0bb1752c96ecd70ab104a438f5c51f4c69
NichoStrick <nicoparedes00@gmail.com>
2025-09-20 19:12:37 -0300

    Clase RegistroTrampas creada con su constructor y ArrayList de trampas hechas

---
+++ src/casinoDados/RegistroTrampas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package casinoDados;
+
+import java.util.ArrayList;
+
+
+class RegistroTrampas {
+    private ArrayList <String> trampas;
+
+    public RegistroTrampas() {
+        this.trampas = new ArrayList<>();
+    }
+    
+    
+    
+}
