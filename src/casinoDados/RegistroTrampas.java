98814e9f40296efb2f30dc68811771bdc481f88d
NichoStrick <nicoparedes00@gmail.com>
2025-09-20 19:19:07 -0300

    Clase RegistroTrampas completa con sus respectivos métodos

--- src/casinoDados/RegistroTrampas.java
+++ src/casinoDados/RegistroTrampas.java
@@ -14,6 +14,20 @@ class RegistroTrampas {
         this.trampas = new ArrayList<>();
     }
     
+    public void registrarTrampa(String tipoDeTrampa, String victima){
+        String registro = tipoDeTrampa + " aplicada a " + victima;
+        trampas.add(registro);
+    }
     
+    public ArrayList<String> obtenerTrampas(){
+        return trampas;
+    }
+    
+    public void mostrarTrampas(){
+        System.out.println("REGISTRO DE TRAMPAS");
+        for(String trampa : trampas){
+            System.out.println("- " + trampa);
+        }
+    }
     
 }
